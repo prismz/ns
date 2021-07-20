be69a00ef9e962ed008e33553e20a2ea1e075e8f
prismz <hasanzahra@tutanota.com>
2021-07-20 16:55:55 -0500

    modified:   uptime.c

--- uptime.c
+++ uptime.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <math.h>
 
 char*
@@ -13,23 +14,24 @@ get_uptime(void)
     char* fc = read_file("/proc/uptime");
     char* info = strtok(fc, " ");
     char* b;
+
     long uptime_sec = strtol(info, &b, 10);
-    double hours = (double)((double)uptime_sec / 60) / 60;
-    double mins = (double)(hours - floor(hours)) * 60;
-    hours = floor(hours);
-    mins = floor(mins);
-    double days = (double)hours / 24;
-    double hourem = (double)days - floor(days);
-    hourem *= 24;
-    days = floor(days);
+
+    int secs_in_hr = 60 * 60;
+
+    int hours = (uptime_sec / secs_in_hr);
+    int days = hours / 24;
+    int rem_sec = uptime_sec - (hours * secs_in_hr);
+    int mins = rem_sec / 60;
+    hours = (hours - (24 * days));   
 
     char* str = smalloc(sizeof(char) * 750);
     if (days > 1)
-        snprintf(str, 750, "up %d days, %02d:%02d", (int)days, (int)hourem, (int)mins);
+        snprintf(str, 750, "up %d days, %02d:%02d", days, hours, mins);
     else if (days == 1)
-        snprintf(str, 750, "up %d day, %02d:%02d", (int)days, (int)hourem, (int)mins);
+        snprintf(str, 750, "up %d day, %02d:%02d", days, hours, mins);
     else
-        snprintf(str, 750, "up %02d:%02d", (int)hourem, (int)mins);
+        snprintf(str, 750, "up %02d:%02d", hours, mins);
 
     free(fc);
     return str;
